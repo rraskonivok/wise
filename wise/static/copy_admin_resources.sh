465d729e63bee98af9c033b47c9002f9d46e9ac6
archone <archone@archone.(none)>
2010-11-13 17:10:02 -0500

    added installation directions

--- wise/static/copy_admin_resources.sh
+++ wise/static/copy_admin_resources.sh
@@ -3,3 +3,4 @@
 echo 'Copying web resources from Django package'
 mkdir admin
 cp -R /usr/lib/python2.6/site-packages/django/contrib/admin/media/* admin/
+#cp -R /usr/lib/python2.7/site-packages/django/contrib/admin/media/* admin/
