287f195135c9cec572488afce191224ff43cac9e
sdiehl <freyrs@gmail.com>
2011-01-06 22:44:07 -0500

    fixed istall for ubuntu 10.10

--- dist/build_pure.sh
+++ dist/build_pure.sh
@@ -2,12 +2,7 @@
 
 VERSION=0.46
 
-dpkg --status libgmp3-dev llvm | grep -q not-installed
-
-if [ $? -eq 0 ]; then
-    sudo apt-get install libgmp3-dev llvm
-fi
-
+sudo apt-get install libgmp3-dev llvm g++
 wget http://pure-lang.googlecode.com/files/pure-$VERSION.tar.gz
 tar xfvz pure-$VERSION.tar.gz
 rm pure-$VERSION.tar.gz
